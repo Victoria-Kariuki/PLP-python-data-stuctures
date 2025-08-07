4e0d87588b047dc310a821f7785e6e3a69050714
Victoria-Kariuki <kariukivictoria428@gmail.com>
2025-08-07 10:42:31 +0300

    data-structures

---
+++ data_structures.py
@@ -0,0 +1,16 @@
+my_list = []
+
+my_list.extend([10, 20, 30, 40])
+
+my_list.insert(1, 15)
+
+my_list.extend([50, 60, 70])
+
+del my_list[-1]
+
+my_list.sort()
+
+index_30 = my_list.index(30)
+print(index_30)
+
+print(my_list)
